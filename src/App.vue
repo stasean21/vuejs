a94150ec8644384f619f94cfb872be09cc9ce4ea
Stanislav Muntyanu <stasean21@yandex.com>
2023-12-18 02:00:07 +0300

    Новые данные

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
+    <div class="page">
+      <button class="show-modal-button" @click="showModal">Показать модальное окно</button>
+      <ModalWindow ref="modal"></ModalWindow>
+    </div>
+  </template>
+  
+  <script>
+  // Используйте относительный путь с префиксом '@'
+  import ModalWindow from './components/Modal-window.vue';
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  
+  export default {
+    name: 'App',
+    components: {
+      ModalWindow,
+    },
+    methods: {
+      showModal() {
+        this.$refs.modal.show = true;
+      },
+    },
+  };
+  </script>
+ 
+<style lang="scss">
+   body {
+  font-family: 'Source Sans Pro', sans-serif;
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.page {
+  position: relative;
+  width: 100%;
+  min-height: 100%;
+}
+
+/* Стили для кнопки */
+
+.show-modal-button {
+  background-color: #0971c7;
+  color: #fff;
+  border: none;
+  padding: 10px 15px;
+  font-size: 16px;
+  border-radius: 8px;
+  cursor: pointer;
+}
+
+.show-modal-button:hover {
+  background-color: #2a4cc7;
 }
 </style>
